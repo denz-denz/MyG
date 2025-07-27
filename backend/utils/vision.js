54c30e924c6345f23bbff757ea1846633d9477ed
denz-denz <E1398671@u.nus.edu>
2025-07-27 23:03:16 +0800

    altered google cloud to fit render

--- backend/utils/vision.js
+++ backend/utils/vision.js
@@ -1,14 +1,18 @@
 const vision = require('@google-cloud/vision');
+let client;
 
-const client = new vision.ImageAnnotatorClient({
-  keyFilename: 'google-vision.json'
-});
+if (process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON) {
+  // Render environment (JSON string)
+  const credentials = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS_JSON);
+  client = new vision.ImageAnnotatorClient({ credentials });
+} else {
+  // Local environment (file on disk)
+  client = new vision.ImageAnnotatorClient({ keyFilename: 'google-vision.json' });
+}
 
-async function detectLabels(base64Image) {
+module.exports = async function detectLabels(base64Image) {
   const [result] = await client.labelDetection({
     image: { content: base64Image }
   });
   return result.labelAnnotations.map(label => label.description);
-}
-
-module.exports = detectLabels;
+};
